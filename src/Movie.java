970cccf789d34faed2b32bf27fe880c71a61a05a
henryhrlim <33216909+henryhrlim@users.noreply.github.com>
2017-10-30 18:45:48 +0800

    commit

---
+++ src/Movie.java
@@ -0,0 +1,19 @@
+public class Movie {
+    private int movieID;
+    private double ratings;
+    private String movieRating;
+    private String duration;
+    private String title;
+    private String synopsis;
+    private String director;
+    private String cast;
+    private String movieType;
+    private String movieStatus;
+    List<Review> reviews;
+
+    public Movie(){
+
+    }
+
+    public Movie(int movieID,
+}
