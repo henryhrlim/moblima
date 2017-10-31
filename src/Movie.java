5b99af6cf9ad8a8d3ff80e1a8c4a27dfbadb6c9a
henryhrlim <33216909+henryhrlim@users.noreply.github.com>
2017-10-31 16:38:59 +0800

    movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -15,5 +15,105 @@ public class Movie {
 
     }
 
-    public Movie(int movieID,
+    public Movie(int movieID, double ratings, String movieRating, String duration, String title, String synopsis, String director, String cast, String movieType, String movieStatus){
+        this.movieID = movieID;
+        this.ratings = ratings;
+        this.movieRating = movieRating;
+        this.duration = duration;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.director = director;
+        this.cast = cast;
+        this.movieType = movieType;
+        this.movieStatus = movieStatus;
+        this.reviews = reviews;
+    }
+
+    public int getMovieID(){
+        return movieID;
+    }
+
+    public void setMovieID(int movieID){
+        this.movieID = movieID;
+    }
+
+    public double getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(double ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getMovieRating() {
+        return movieRating;
+    }
+
+    public void setMovieRating(String movieRating) {
+        this.movieRating = movieRating;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(String movieType) {
+        this.movieType = movieType;
+    }
+
+    public String getMovieStatus() {
+        return movieStatus;
+    }
+
+    public void setMovieStatus(String movieStatus) {
+        this.movieStatus = movieStatus;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
