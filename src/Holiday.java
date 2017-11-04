9b48cbed05d58d61702d140fefe22ac7a9ba4512
ahminn <33260194+ahminn@users.noreply.github.com>
2017-11-04 21:46:04 +0800

    commit

---
+++ src/Holiday.java
@@ -0,0 +1,31 @@
+
+public class Holiday {
+	private String date;
+	private String holidayName;
+	
+	public Holiday(){
+		
+	}
+	
+	public Holiday(String date, String holidayName){
+		this.date = date;
+		this.holidayName = holidayName;
+	}
+	
+	public String getDate(){
+		return date;
+	}
+	
+	public void setDate(String date){
+		this.date = date;
+	}
+	
+	public String getHolidayName(){
+		return holidayName;
+	}
+	
+	public void setHolidayName(String holidayName){
+		this.holidayName = holidayName;
+	}
+
+}
---
+++ src/Tickets.java
@@ -0,0 +1,31 @@
+
+public class Tickets {
+	private int ticketID;
+	private String age;
+	
+	public Tickets(){
+		
+	}
+	
+	public Tickets(int ticketID, String age){
+		this.ticketID = ticketID;
+		this.age = age;
+	}
+	
+	public int getTicketID(){
+		return ticketID;
+	}
+	
+	public void setTicketID(int ticketID){
+		this.ticketID = ticketID;
+	}
+	
+	public String getAge(){
+		return age;
+	}
+	
+	public void setAge(String age){
+		this.age = age;
+	}
+
+}
