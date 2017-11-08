7e347a591d382afb9b0d5a9d7b175430114e6db9
nanoduckie <yuennicholas@icloud.com>
2017-11-09 00:34:24 +0800

    StaffController
    authenticate() and staffMenuController()

--- src/controllers/StaffController.java
+++ src/controllers/StaffController.java
@@ -2,22 +2,78 @@ package controllers;
 
 import java.util.Scanner;
 
+import objects.Cineplex;
+
 public class StaffController {
 	private StaffMenu s;
 	private MoblimaApp app;
+	Scanner sc = new Scanner(System.in);
 	
+//	Return true if successfully authenticated and false if credentials are wrong
 	public boolean authenticate() {
-		boolean locked = true;
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Username: ");
 		char username[] = sc.nextLine();
 		System.out.println("Password: ");
 		char password[] = sc.nextLine();
-		
+		if (username.equals("admin") && password.equals("admin")) {
+			System.out.println("Logged in.");
+			return true;
+		}
+		else
+			return false;
 	}
 	
+//	Prints cineplex menu
 	public void staffMenuCineplex() {
+		int choice;
 		
+		do {
+			System.out.println("================= Cineplex Menu =================\n"
+					+ "1. List Cineplex\n"
+					+ "2. Show Movies in Cineplex\n"
+					+ "3. Add Cineplex\n"
+					+ "4. Add Movies to Cineplex\n"
+					+ "5. Remove Movies from Cineplex\n"
+					+ "6. Back\n"
+					+ "7. Quit\n"
+					+ "Please select an option: ");
+			choice = sc.nextInt();
+			switch (choice) {
+				case 1:
+					displayCineplexList();
+					break;
+				case 2:
+					StaffController staffControl = new StaffController();
+					CineplexController cineplexControl = new CineplexController();
+					Cineplex cineUserChoice = cineplexControl.listCineplex();
+					staffControl.listMovieSpecificToCineplex(cineUserChoice);
+					break;
+				case 3:
+					addCineplex();
+					break;
+				case 4:
+					addMovieToCineplex();
+					break;
+				case 5:
+					removeMovieFromCineplex();
+					break;
+				case 6:
+				case 7:
+					break;
+				default:
+					System.out.println("Invalid option. Please enter an integer between 1 to 7");
+					break;
+			}
+		} while (choice != 6 && choice != 7);
+		
+		if (choice == 6)
+			s.show();
+		else if (choice == 7) {
+			System.out.println("Exiting...");
+			sc.close();
+			System.exit(0);
+			break;
+		}
 	}
 	
 	public void displayCineplexList() {
@@ -219,4 +275,6 @@ public class StaffController {
 	 public static List<PriceChart> filterListPriceChart(String ageGrp) {
 		 
 	 }
+	 
+	 sc.close();
 }
