b5b0db5a5b0c60e8ae7c91f730543a8077c7bbaa
nanoduckie <yuennicholas@icloud.com>
2017-11-08 20:33:14 +0800

    Changes

---
+++ src/StaffController.java
@@ -0,0 +1,146 @@
+
+public class StaffController {
+	private static StaffMenu s;
+	private static MoblimaApp app;
+	
+	public boolean authenticate() {
+		
+	}
+	
+	public void staffMenuCineplex() {
+		
+	}
+	
+	public void displayCineplexList() {
+		
+	}
+	
+	 public void addCineplex() {
+		 
+	 }
+	 
+	 public void addMovieToCineplex() {
+		 
+	 }
+	  
+	 public void listMovieSpecificToCineplex(Cineplex userChoice) {
+		 
+	 }
+	 
+	 public void removeMovieFromCineplex() {
+		 
+	 }
+	 
+	 public void addShowTimeToCineplex() {
+		 
+	 }
+	 
+	 public void removeShowTimeFromCineplex() {
+		 
+	 }
+	 
+	 public void addCinemaToCineplex(Cineplex userChoice) {
+		 
+	 }
+	 
+	 public void removeCinemaFromCineplex() {
+		 
+	 }
+	 
+	 public void listShowTimeSpecificToCineplex(Cineplex userChoice) {
+		 
+	 }
+	 
+	 public void staffMenuCinema() {
+		 
+	 }
+	 
+	 public void displayCinemaList() {
+		 
+	 }
+	 
+	 public void updateCinemaInCineplex() {
+		 
+	 }
+	 
+	 public void holidayMenu() {
+		 
+	 }
+	 
+	 public char retrieveList() {
+		 char list[];
+		 return list;
+	 }
+	 
+	 public void staffMenuHoliday(int choice) {
+		 
+	 }
+	 
+	 public void listHoliday() {
+		 
+	 }
+	 
+	 public void addHoliday() {
+		 
+	 }
+	 
+	 public void updateHoliday() {
+		 
+	 }
+	 
+	 public void deleteHoliday() {
+		 
+	 }
+	 
+	 public void printHolidayMenu() {
+		 
+	 }
+	 
+	 public void staffMenuMovie() {
+		 
+	 }
+	 
+	 public void displayMovieList() {
+		 
+	 }
+	 
+	 public void updateMovieList() {
+		 
+	 }
+	 
+	 public void addMovieToList() {
+		 
+	 }
+	 
+	 public void removeMovieList() {
+		 
+	 }
+	 
+	 public void showTimeMain() {
+		 
+	 }
+	 
+	 public void staffMenuShowTime(int choice) {
+		 
+	 }
+	 
+	 public void listShowTime() {
+		 
+	 }
+	 
+	 public void listByMovie() {
+		 
+	 }
+	 
+	 public void listByCinema() {
+		 
+	 }
+	 
+	 public void addShowTime() {
+		 
+	 }
+	 
+	 public void updateShowTime() {
+		 
+	 }
+}
---
+++ src/StaffMenu.java
@@ -0,0 +1,6 @@
+
+public class StaffMenu {
+	public show() {
+		
+	}
+}
